7f271328ae7d17cf6ceadfe8afbb099b8f21efd3
ruhul1845 <rohul4316@gmail.com>
2026-03-31 07:31:47 +0600

    Usercard Modified

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
+import user from './assets/user.png'
+import rocket from './assets/rocket.png'
+import pack from './assets/package.png'
 
 const UserCard = () => {
     return (
-        <div className="flex gap-4 p-6  rounded-xl">
+        <div className="flex gap-4 p-10  rounded-xl">
 
 
-            <div className="relative flex-1 flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl shadow-lg">
-                <span className="absolute -top-4 right-4 bg-violet-600 text-white text-xs font-bold w-8 h-8 rounded-full flex items-center justify-center">
+            <div className="relative w-[250px] h-[300px] flex-1 flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl shadow-lg">
+                <span className="absolute top-4 right-4 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white text-xs font-bold w-10 h-10 rounded-full flex items-center justify-center">
                     01
                 </span>
-                <div className="w-16 h-16 bg-violet-100 rounded-full flex items-center justify-center text-2xl mb-4">
-                    👤
+                <div className='w-25 h-25 bg-[#EDD9FF] flex items-center justify-center rounded-full mb-4'>
+                    <img src={user} alt="user" />
                 </div>
-                <h3 className="text-lg font-bold text-gray-900 mb-2">Create Account</h3>
-                <p className="text-sm text-gray-500 leading-relaxed">
+                <h3 className="text-xl font-black mb-2">Create Account</h3>
+                <p className="text-base text-gray-500 leading-6 max-w-[350px]">
                     Sign up for free in seconds. No credit card required to get started.
                 </p>
             </div>
 
-            <div className="relative shadow-lg flex-1  flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl">
-                <span className="absolute -top-4 right-4 bg-violet-600 text-white text-xs font-bold w-8 h-8 rounded-full flex items-center justify-center">
+            <div className="relative w-[250px] h-[300px] shadow-lg flex-1  flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl">
+                <span className="absolute top-4 right-4 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white text-xs font-bold w-10 h-10 rounded-full flex items-center justify-center">
                     02
                 </span>
-                <div className="w-16 h-16 bg-violet-100 rounded-full flex items-center justify-center text-2xl mb-4">
-                    📦
+                <div className='w-25 h-25  rounded-full bg-[#EDD9FF] mb-4 flex items-center justify-center'>
+                    <img src={pack} alt="package" />
                 </div>
-                <h3 className="text-lg font-bold text-gray-900 mb-2">Choose Products</h3>
-                <p className="text-sm text-gray-500 leading-relaxed">
+                <h3 className="text-xl font-black mb-2">Choose Products</h3>
+                <p className="text-base text-gray-500 leading-6 max-w-[350px]">
                     Browse our catalog and select the tools that fit your needs.
                 </p>
             </div>
 
 
-            <div className="relative shadow-lg flex-1 flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl">
-                <span className="absolute -top-4 right-4 bg-violet-600 text-white text-xs font-bold w-8 h-8 rounded-full flex items-center justify-center">
+            <div className="relative w-[250px] h-[300px] shadow-lg flex-1 flex flex-col items-center text-center p-6 border border-gray-200 rounded-xl">
+                <span className="absolute top-4 right-4 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white text-xs font-bold w-10 h-10 rounded-full flex items-center justify-center">
                     03
                 </span>
-                <div className="w-16 h-16 bg-violet-100 rounded-full flex items-center justify-center text-2xl mb-4">
-                    🚀
+                <div className='w-25 h-25 rounded-full bg-[#EDD9FF] flex items-center justify-center mb-4'>
+                    <img src={rocket} alt="rocket" />
                 </div>
-                <h3 className="text-lg font-bold text-gray-900 mb-2">Start Creating</h3>
-                <p className="text-sm text-gray-500 leading-relaxed">
+                <h3 className="text-xl font-black mb-2">Start Creating</h3>
+                <p className="text-base leading-6 max-w-[350px] text-gray-500 ">
                     Download and start using your premium tools immediately.
                 </p>
             </div>
